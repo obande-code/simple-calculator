16a43959b3df1a9bb79c6fe7713e9451b7693a0b
James Obande <Jamesobande2015@gmail.com>
2020-02-25 13:51:50 +0100

    first commit

---
+++ calculator.js
@@ -0,0 +1,10 @@
+function number(value){
+		document.form1.result.value +=value;
+	}
+	function cle(value){
+		document.form1.result.value =value;
+	}
+	function evalua(){
+		//var evalu = eval(document.form1.result.value);
+		document.form1.result.value = eval(document.form1.result.value);
+	}
